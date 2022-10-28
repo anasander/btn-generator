5a1633bef3c620f56b2a2b2c5baed0d036185448
Ana Sander <anagsander@gmail.com>
2022-10-28 13:40:56 -0300

    btn-gen JS

---
+++ script.js
@@ -0,0 +1,65 @@
+const controles = document.getElementById("controls");
+const cssTexto = document.querySelector(".css");
+const btn = document.querySelector(".btn");
+
+controles.addEventListener("change", handleChange);
+
+const handleStyle = {
+  element: btn,
+  backgroundColor(value) {
+    this.element.style.backgroundColor = value;
+  },
+  height(value) {
+    this.element.style.height = value + "px";
+  },
+  width(value) {
+    this.element.style.width = value + "px";
+  },
+  text(value) {
+    this.element.innerText = value;
+  },
+  color(value) {
+    this.element.style.color = value;
+  },
+  border(value) {
+    this.element.style.border = value;
+  },
+  borderRadius(value) {
+    this.element.style.borderRadius = value + "px";
+  },
+  fontFamily(value) {
+    this.element.style.fontFamily = value;
+  },
+  fontSize(value) {
+    this.element.style.fontSize = value + "rem";
+  },
+};
+
+function handleChange(event) {
+  const name = event.target.name;
+  const value = event.target.value;
+
+  handleStyle[name](value);
+  saveValues(name, value);
+  showCSS();
+}
+
+function saveValues(name, value) {
+  localStorage[name] = value;
+}
+
+function setValues() {
+  const properties = Object.keys(localStorage);
+  properties.forEach((propertie) => {
+    handleStyle[propertie](localStorage[propertie]);
+    controles.elements[propertie].value = localStorage[propertie];
+  });
+  showCSS();
+}
+
+setValues();
+
+function showCSS() {
+  cssTexto.innerHTML =
+    "<span>" + btn.style.cssText.split("; ").join(";</span><span>");
+}
